d894b0000a9186f812b47939bcc5562e7625e02b
siansiong97 <siansiong5@gmail.com>
2019-10-28 22:23:37 +0800

    Company Inventory CRUD with super messy but workable version,  it just like i throw a rubbish assignment at last  minute

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -6,17 +6,29 @@ namespace App\Http\Controllers\API;
 use App\Company;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Traits\GlobalFunctions;
+use App\Traits\NotificationFunctions;
+use DB;
+use Carbon\Carbon;
 
 class CompanyController extends Controller
 {
+    use GlobalFunctions, NotificationFunctions;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $companies = Company::where('status' , true)->get();
+        //Page Pagination Result List
+        //Default return 10
+        $paginateddata = $this->paginateResult($companies , $request->result, $request->page);
+        $data['data'] = $paginateddata;
+        $data['maximunPage'] = $this->getMaximumPaginationPage($companies->count(), $request->result);
+        $data['msg'] = $this->getRetrievedSuccessMsg('Companies');
+        return response()->json($data, 200);
     }
 
     /**
@@ -37,7 +49,7 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -46,9 +58,22 @@ class CompanyController extends Controller
      * @param  \App\Company  $company
      * @return \Illuminate\Http\Response
      */
-    public function show(Company $company)
+    public function show($uid)
     {
-        //
+        $company = Company::where('uid',$uid)->with(['companytype' => function($q){
+            $q->where('status', 1);
+        }])->first();
+    
+        if(empty($company) || $company->companytype == null){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Company Not Found.';
+            return response()->json($payload, 404);
+        }
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Company Retrieved.';
+        $payload['data'] = $company;
+
+        return response()->json($payload, 200);
     }
 
     /**
@@ -69,9 +94,97 @@ class CompanyController extends Controller
      * @param  \App\Company  $company
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Company $company)
+    public function update(Request $request, $uid)
     {
-        //
+        $company = Company::where('uid',$uid)->where('status', true)->first();
+        if(empty($company)){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Company not found.';
+            return response()->json($payload, 404);
+        }
+
+        $this->validate($request, [
+            'name' => 'required|string|max:191',
+            'regno' => 'string|nullable',
+        ]);
+
+        // $companytype = CompanyType::where('name', $request->company_type)->first();
+        // if(empty($companytype)){
+        //     $payload['status'] = 'error';
+        //     $payload['msg'] = 'Company Type Not Found.';
+
+        //     return response()->json($payload, 404);
+        // }
+        // $company->companytype()->associate($companytype);
+
+        $company->name = $request->name;
+        $company->regno = $request->regno;
+        $company->fax1 = $request->fax1;
+        $company->fax2 = $request->fax2;
+        $company->email1 = $request->email1;
+        $company->email2 = $request->email2;
+        $company->address1 = $request->address1;
+        $company->address2 = $request->address2;
+        $company->postcode = $request->postcode;
+        $company->city = $request->city;
+        $company->state = $request->state;
+        $company->country = $request->country;
+        $company->status = true;
+        $company->lastedit_by = $request->user()->name;
+
+         try{
+            $company->save();
+        }catch(Exception $e){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Company cannot be update.';
+
+            return response()->json($payload, 500);
+        }
+        // $account = Account::where('id', $request->account['id'])->first();
+        // if(empty($account)){
+        //     $payload['status'] = 'error';
+        //     $payload['msg'] = 'Account Not Found.';
+
+        //     return response()->json($payload, 404);
+        // }
+
+        // $account->fname = $request->fname;
+        // $account->lname = $request->lname;
+        // $account->email = $request->email1;
+        // $account->icno = $request->icno;
+        // $account->tel1 = $request->tel1;
+        // $account->tel2 = $request->tel2;
+        
+        // $account->company()->associate($company);
+        
+        // try{
+        //     $account->save();
+        // }catch(Exception $e){
+        //     DB::rollBack();
+        //     $payload['status'] = 'error';
+        //     $payload['msg'] = 'Account cannot be update.';
+
+        //     return response()->json($payload, 500);
+        // }
+        // $group= Group::where('company_id', $request->id)->first();
+        // $group->name = $company->name . '\'s default group';
+        // $group->company()->associate($company);
+        // try{
+        //     $group->save();
+        // }catch(Exception $e){
+        //     DB::rollBack();
+        //     $payload['status'] = 'error';
+        //     $payload['msg'] = 'Group cannot be update.';
+
+        //     return response()->json($payload, 500);
+        // }
+
+        DB::commit();
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Company successfully updated.';
+        $payload['data'] =  $company->refresh();
+
+        return response()->json($payload, 200);
     }
 
     /**
--- app/Http/Controllers/API/InventoryController.php
+++ app/Http/Controllers/API/InventoryController.php
@@ -5,17 +5,29 @@ namespace App\Http\Controllers\API;
 use App\Inventory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Traits\GlobalFunctions;
+use App\Traits\NotificationFunctions;
+use DB;
+use Carbon\Carbon;
 
 class InventoryController extends Controller
 {
+    use GlobalFunctions, NotificationFunctions;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $inventories = Inventory::where('status' , true)->get();
+        //Page Pagination Result List
+        //Default return 10
+        $paginateddata = $this->paginateResult($inventories , $request->result, $request->page);
+        $data['data'] = $paginateddata;
+        $data['maximunPage'] = $this->getMaximumPaginationPage($inventories->count(), $request->result);
+        $data['msg'] = $this->getRetrievedSuccessMsg('Inventories');
+        return response()->json($data, 200);
     }
 
     /**
@@ -36,7 +48,111 @@ class InventoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         
+        // $this->validate($request, [
+        //     'company_id' => 'required',
+        //     'name' => 'required|string|max:191',
+        //     'code' => 'nullable',
+        //     'sku' => 'required|string|max:191',
+        //     'cost' => 'required|numeric|min:0',
+        //     'price' => 'required|numeric|min:0',
+        //     'stock' => 'required|numeric|min:0',
+        //     'backorder' => 'required|boolean',
+        //     'stockthreshold' => 'required|numeric|min:0',
+        // ]);
+        if(Inventory::where('company_id',$request->company_id)->where('code',$request->code)->count() > 0 || Inventory::where('company_id',$request->company_id)->where('sku',$request->sku)->count() > 0 ){
+            
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Code / Sku has been used.';
+
+            return response()->json($payload, 404);
+        }
+
+        DB::beginTransaction();
+
+        $inventory = new Inventory();
+        $checkid = false;
+        $uid = '';
+        while(!$checkid){
+            $uid = '4' . Carbon::now()->timestamp;
+            if (!Inventory::where('uid', '=', $uid)->exists()) {
+                // user found
+                $checkid = true;
+            }
+        }
+
+        $inventory->uid = $uid;
+        $inventory->code = $request->code;
+        $inventory->name = $request->name;
+        $inventory->sku = $request->sku;
+        $inventory->cost = $request->cost;
+        $inventory->price = $request->price;
+        $inventory->desc = $request->desc;
+        $inventory->stock = $request->stock;
+        $inventory->backorder = $request->backorder;
+        $inventory->stockthreshold = $request->stockthreshold;
+        $inventory->salesqty = 0;
+        // $company = Company::find($request->company_id);
+        $company = Company::find(1);
+        if(empty($company)){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Company Not Found.';
+
+            return response()->json($payload, 404);
+        }
+
+        $inventory->company()->associate($company);
+        if(!$inventory->save()){
+            DB::rollBack();
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Inventory Cannot Save.';
+
+            return response()->json($payload, 404);
+        }
+
+        // if($inventory->stock > 0){
+
+        //     $batch = new Batch();
+        //     $batch->uid = $inventory->uid.'-'.($inventory->batches()->where('status','!=','cancel')->count() + 1);
+        //     $batch->cost = $inventory->cost;
+        //     $batch->price = $inventory->price;
+        //     $batch->stock = $inventory->stock;
+        //     $batch->salesqty = $inventory->salesqty;
+        //     $batch->batchno = $inventory->batches()->where('status', true)->count() + 1;
+        //     $batch->curbatch = true;
+        //     $batch->inventory()->associate($inventory);
+
+            
+        //     if(!$batch->save()){
+        //         DB::rollBack();
+        //         $payload['status'] = 'error';
+        //         $payload['msg'] = 'Batch Cannot Save.';
+
+        //         return response()->json($payload, 404);
+        //     }
+        // }
+
+        
+        // $oriinventory = Inventory::where('sku', $request->sku)->where('company_id',1)->first();
+        // if(!empty($oriinventory)){
+        //     //Price Lists
+        //     $accounts = $oriinventory->accounts;
+        //     $inventory = $inventory->refresh();
+        //     $inventory->accounts()->detach();
+        //     foreach($accounts as $account){
+        //         $inventory->accounts()->attach($account->id , ['min' => $account->pivot->min, 'price' => $account->pivot->price]);
+        //     }
+        // }
+
+
+        DB::commit();
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Inventory Saved.';
+        $payload['inventory'] = $inventory->refresh();
+
+        return response()->json($payload, 200);
+
+
     }
 
     /**
@@ -45,9 +161,23 @@ class InventoryController extends Controller
      * @param  \App\Inventory  $inventory
      * @return \Illuminate\Http\Response
      */
-    public function show(Inventory $inventory)
+    public function show($uid)
     {
-        //
+        try{
+            
+            $inventory = Inventory::where('uid', $uid)->with('company')->first();
+        }catch(Exception $e){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Error 404';
+
+            return response()->json($payload, 404);
+        }
+        
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Inventories Retrieved Success.';
+        $payload['data'] = $inventory;
+
+        return response()->json($payload, 200);
     }
 
     /**
@@ -68,9 +198,78 @@ class InventoryController extends Controller
      * @param  \App\Inventory  $inventory
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Inventory $inventory)
+    public function update(Request $request, $uid)
     {
-        //
+        $inventory = Inventory::where('uid',$uid)->where('status',true)->first();
+        if(empty($inventory)){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Inventory Not Found.';
+
+            return response()->json($payload, 404);
+        }
+        // $this->validate($request, [
+        //     'company_id' => 'required',
+        //     'code' => 'nullable|string|max:191',
+        //     'sku' => 'required|string|max:191',
+        //     'name' => 'required|string|max:191',
+        //     'cost' => 'required|numeric|min:0',
+        //     'price' => 'required|numeric|min:0',
+        //     'backorder' => 'required|boolean',
+        //     'stockthreshold' => 'required|numeric|min:0',
+        // ]);
+        
+        
+        if($inventory->sku != $request->sku){
+            if(Inventory::where('company_id',$request->company_id)->where('code',$request->code)->count() > 0 || Inventory::where('company_id',$request->company_id)->where('sku',$request->sku)->count() > 0 ){
+            
+                $payload['status'] = 'error';
+                $payload['msg'] = 'Code / Sku has been used.';
+    
+                return response()->json($payload, 404);
+            }
+        }
+        $inventory->code = $request->code;
+        $inventory->name = $request->name;
+        $inventory->sku = $request->sku;
+        $inventory->cost = $request->cost;
+        $inventory->price = $request->price;
+        $inventory->desc = $request->desc;
+        $inventory->backorder = $request->backorder;
+        $inventory->stockthreshold = $request->stockthreshold;
+
+        // $company = Company::find($request->company_id);
+
+        // if(empty($company)){
+        //     $payload['status'] = 'error';
+        //     $payload['msg'] = 'Company Not Found.';
+
+        //     return response()->json($payload, 404);
+        // }
+        // $inventory->company()->associate($company);
+        if(!$inventory->save()){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Inventory Cannot Save.';
+
+            return response()->json($payload, 404);
+        }
+
+        
+        // $oriinventory = Inventory::where('sku', $request->sku)->where('company_id',1)->first();
+        // if(!empty($oriinventory)){
+        //     //Price Lists
+        //     $accounts = $oriinventory->accounts;
+        //     $inventory = $inventory->refresh();
+        //     $inventory->accounts()->detach();
+        //     foreach($accounts as $account){
+        //         $inventory->accounts()->attach($account->id , ['min' => $account->pivot->min, 'price' => $account->pivot->price]);
+        //     }
+        // }
+
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Inventories Updated Success.';
+        $payload['data'] = $inventory;
+
+        return response()->json($payload, 200);
     }
 
     /**
@@ -79,8 +278,43 @@ class InventoryController extends Controller
      * @param  \App\Inventory  $inventory
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Inventory $inventory)
+    public function destroy(Request $request, $uid)
     {
-        //
+         $inventory = Inventory::where('uid',$uid)->where('status', true)->first();
+        if(empty($inventory)){
+            $payload['status'] = 'error';
+            $payload['msg'] = 'Inventory Not Found.';
+
+            return response()->json($payload, 404);
+        }
+        // $batches = $inventory->batches;
+
+        $inventory->status = false;
+
+        // DB::beginTransaction();
+        // foreach($batches as $batch){
+        //     if(!empty($batch->purchaseitem) || !$batch->saleitems->isEmpty()){
+        //         error_log($batch->purchaseitem);
+        //         error_log($batch->saleitems);
+
+        //         DB::rollBack();
+        //         $payload['status'] = 'error';
+        //         $payload['msg'] = 'Cannot Delete This Inventory! Because it had been used in others operation.';
+
+        //         return response()->json($payload, 404);
+        //     }else{
+        //         $batch->delete();
+        //     }
+        // }
+
+        $inventory->save();
+
+        // DB::commit();
+
+        $payload['status'] = 'success';
+        $payload['msg'] = 'Inventory Delete Successful.';
+        $payload['data'] = $inventory;
+
+        return response()->json($payload, 200);
     }
 }

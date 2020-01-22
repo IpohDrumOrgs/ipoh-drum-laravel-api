7c27cfa634189b4bf9f153b0a00859d3275a848b
henrylow97 <henry_lcz97@hotmail.com>
2020-01-23 01:26:42 +0800

    -Development commit.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,7 +19,7 @@ use Illuminate\Routing\Controller as BaseController;
  */
 /**
  *  @OA\Server(
- *      url="http://localhost:8000/",
+ *      url="http://172.104.45.205/",
  *      description="The URL that IpohDrum Laravel API is running on."
  *  )
  */
